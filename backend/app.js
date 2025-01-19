6dcdf0e686e01c93f289007e3d1e9e9093a70f93
Chandrakant@121 <csjoshi989@gmail.com>
2025-01-19 18:25:43 +0530

    user registration api

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const dotenv = require('dotenv')
 const cors = require('cors')
-dotenv.config()
 const express = require('express')
+const connect = require('./db/db')
+const userRoutes = require('./routes/user.routes')
+
 const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+connect()
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.get('Hello world')
 })
+
+app.use('/users', userRoutes)
 
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const http = require('http')
 const app = require('./app')
 const port = process.env.PORT || 3000
@@ -7,4 +6,19 @@ const server = http.createServer(app)
 server.listen(port, () => {
     console.log(`server running on ${port}`)
 })
+
+
+
+
+
+
+// const connect = require('./db/db')
+// app.listen(port, async () => {
+//     try {
+//         await connect();
+//         console.log("listening port 5000");
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// });
 
